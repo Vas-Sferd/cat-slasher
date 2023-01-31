903be811a3f98a5756ea6e51ef8cffe02e0fc4be
Vas Sferd <vassferd@gmail.com>
2023-01-31 15:02:23 +0300

    Немного изменений в коде масштиабирования мира, но это все еще не работает

--- scripts/World.cs
+++ scripts/World.cs
@@ -3,19 +3,32 @@ using System;
 
 public class World : Control
 {
-    [Export]
-    public double position = 0.5;
+	[Export]
+	public double position = 0.5;
+	private Node2D WorldBackground;
 
-    public override void _Ready()
-    {
-        var viewport = GetViewport();
-        viewport.Connect("size_changed", this, "_Resize");
-    }
+	public override void _Ready()
+	{
+		WorldBackground = GetNode<Node2D>("WorldBackground");
+		
+		var viewport = GetViewport();
+		viewport.Connect("size_changed", this, "_Resize");
+		_Resize();
+	}
 
-    public void _Resize()
-    {
-        // Todo доделать масштабирование, используя 
-        // https://godotforums.org/d/20245-help-with-viewport-and-multiple-resolutions/2
-        GD.Print("Size changed");
-    }
+	public void _Resize()
+	{
+		var windowAspectRatio = OS.WindowSize.x / OS.WindowSize.y;
+		var backgroundAspectRatio = WorldBackground.Position.x / WorldBackground.Position.y;
+		
+		if (windowAspectRatio < backgroundAspectRatio)
+		{
+			GD.Print("Size changed here");
+		}
+		
+		var newSize = OS.WindowSize * (1 / windowAspectRatio);
+		GD.Print($"{newSize}");
+		
+		GetViewport().SetSizeOverride(true, newSize);
+	}
 }
